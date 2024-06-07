3d446740738720fe3dc092dba6ca8b18e81da75c
fedorovay <gbpleq1@gmail.com>
2024-06-07 17:57:13 +0700

    07.06.2024

--- frontend-veslo/src/app/(admin)/todo/page.tsx
+++ frontend-veslo/src/app/(admin)/todo/page.tsx
@@ -1,10 +1,10 @@
 
 
 export default async function ToDoPage() {
-  return  <div className="flex flex-wrap">
+  return  <div className="flex  flex-col">
             <h1>My Todo List</h1>
-            <>В order-work-place добавить контекст и прокинуть по потомкам</>
-            <>В модели Order БД добавить адресата (recipient) и привязать заявки к конкретному юзеру</>
-            <>Типизировать код фронта, сбилдить и добавить в докер образ</>
+            <div>В order-work-place добавить контекст и прокинуть по потомкам</div>
+            <div>В модели Order БД добавить адресата (recipient) и привязать заявки к конкретному юзеру</div>
+            <div>Типизировать код фронта, сбилдить и добавить в докер образ</div>
           </div>
 }
