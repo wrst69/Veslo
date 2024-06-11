3b53c700121ea6e155e19c4b5769cd0f92c26c6a
fedorovay <gbpleq1@gmail.com>
2024-06-11 12:19:37 +0700

    cookie env

--- backend-veslo/src/auth/cookie.service.ts
+++ backend-veslo/src/auth/cookie.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Response } from 'express';
 
 @Injectable()
 export class CookieService {
+  constructor(private configService: ConfigService) {}
   static tokenKey = 'access-token';
 
   setToken(res: Response, token: string) {
     res.cookie(CookieService.tokenKey, token, {
       httpOnly: true,
       maxAge: 900000 * 4, //1h in ms
-      sameSite: 'none',
-      secure: true,
+      sameSite: this.configService.get('COOKIE_SAMESITE_OPTION'),
+      secure: false,
 
       /* // true if production
       secure: true,
